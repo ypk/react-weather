1be2c1cf8419ef5b58a80c75d75b981d2caaa8c2
Prasanna K Yalala <yprasannakumar@gmail.com>
2016-11-20 23:15:44 +0000

    Prep for Heroku - Added start parameter in package.json - Added middleware logic to forward https to http - Added env port support

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var express = require('express');
 
 var server = express();
 
-var port = 8080;
+const PORT = process.env.PORT || 8080;
 
 var directory = "public";
 
 var path = express.static(directory);
 
+server.use(function(request, response, next) {
+    if (request.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        response.redirect('http://' + request.hostname + request.url);
+    }
+});
+
 server.use(path);
 
-server.listen(port, function() {
-    console.log("Server started on port: " + port);
+server.listen(PORT, function() {
+    console.log("Server started on port: " + PORT);
 });
