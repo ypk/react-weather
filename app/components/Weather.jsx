222880589f628a40deb4767006d851c0af1635ce
Prasanna K Yalala <yprasannakumar@gmail.com>
2016-11-22 19:12:14 +0000

    added Geolocation for test

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,13 +1,33 @@
 var React = require('react');
+var geolocated = require('react-geolocated');
+
 
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
 var ErrorModal = require('ErrorModal');
 
+
 var Weather = React.createClass({
     getInitialState: function() {
         return {isLoading: false}
+    },
+    getGeoData: function(){
+      return !this.props.isGeolocationAvailable
+      ? <div>Your browser does not support Geolocation</div>
+      : !this.props.isGeolocationEnabled
+        ? <div>Geolocation is not enabled</div>
+        : this.props.coords
+          ? <table>
+            <tbody>
+              <tr><td>latitude</td><td>{this.props.coords.latitude}</td></tr>
+              <tr><td>longitude</td><td>{this.props.coords.longitude}</td></tr>
+              <tr><td>altitude</td><td>{this.props.coords.altitude}</td></tr>
+              <tr><td>heading</td><td>{this.props.coords.heading}</td></tr>
+              <tr><td>speed</td><td>{this.props.coords.speed}</td></tr>
+            </tbody>
+          </table>
+          : <div>Getting the location data&hellip; </div>;
     },
     handleSearch: function(location) {
         this.setState({location: undefined, temp: undefined, isLoading: true, errorMessage: undefined});
@@ -43,6 +63,7 @@ var Weather = React.createClass({
         }
         return (
             <div>
+                {this.getGeoData()}
                 <WeatherForm onSearch={this.handleSearch}></WeatherForm>
                 <div className="text-center">
                     {renderMessage()}
